ae966611412f8e67a7216d5e9de9aeb3db3838e1
Thiago Lehmkuhl Luciano <thiagolehmkuhl42@gmail.com>
2021-05-31 11:41:37 -0300

    testing pycharm

--- Objects_classes_tim/Video3.py
+++ Objects_classes_tim/Video3.py
@@ -36,3 +36,36 @@ jim.speak()
 jim.talk()
 """
 
+
+class Veichle:
+    def __init__(self, price, gas, color):
+        self.price = price
+        self.gas = gas
+        self.color = color
+
+    def fillUpTank(self):
+        self.gas = 100
+
+    def emptyTank(self):
+        self.gas = 0
+
+    def gasleft(self):
+        return self.gas
+
+
+class Car(Veichle):
+    def __init__(self, price, gas, color, speed):
+        super().__init__(price, gas, color)
+        self.speed = speed
+
+    def beep(self):
+        print("Beep beep")
+
+
+class Truck(Veichle):
+    def __init__(self, price, gas, color, tires):
+        super().__init__(price, gas, color)
+        self.tires = tires
+
+    def beep(self):
+        print("Honk")
